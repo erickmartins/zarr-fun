6a4a32579c87dc23355eea9a0a90ca5d19fbe452
Erick Martins Ratamero <erickmartins@gmx.net>
2021-07-09 10:46:04 -0400

    added print tree and attrs

--- test_open.py
+++ test_open.py
@@ -2,7 +2,9 @@ import zarr
 import napari
 
 zobj = zarr.open('https://s3-server.org/bucket-name/zarr-name', mode='r')
-print(zobj['1/1/10/0'].info)
+print (zobj.attrs.asdict()['plate'])
+print(zobj.tree())
+print(zobj['3/7/24/1'].info)
 
 viewer = napari.view_image(zobj['1/1/10/0'])
 napari.run()
