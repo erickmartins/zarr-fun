cb2fcb55240f1891655d442d895bf8988ca25616
Erick Martins Ratamero <erickmartins@gmx.net>
2021-07-14 15:22:11 -0400

    added a few dask things

--- test_dask.py
+++ test_dask.py
@@ -1,24 +1,43 @@
 from dask import delayed
 from dask import array as da
+import dask_image.ndfilters
+from dask.distributed import Client
 import zarr
 import napari
-import napari.utils.io as nio
-import numpy as np
+from skimage.filters import gaussian
+import timeit
 
-zobj = zarr.open('data/example.zarr', mode='w', shape=(1000, 1000, 1000),chunks=(100, 100, 100), dtype='i4')
-[_, _, zobj[:, :, :]] = np.meshgrid(np.arange(1000),np.arange(1000),np.arange(1000))
 
-print(zobj.shape)
 
-#the next two commented examples should work if our zarrs had .zarray...
+if __name__ == "__main__":
+    #run this bit first time around
+    # zobj = zarr.open('data/example.zarr', mode='w', shape=(1000, 1000, 1000),chunks=(100, 100, 100), dtype='i4')
+    # [_, _, zobj[:, :, :]] = np.meshgrid(np.arange(1000),np.arange(1000),np.arange(1000))
 
-# tst = nio.read_zarr_dataset('https://s3-far.jax.org/zarrtest/test7.zarr')
-# print(tst)
+    client = Client(n_workers=4)
 
-img = da.from_zarr(zobj)
-img2 = da.from_zarr('s3-server.org/bucket-name/zarr-name/1/1/10/0')
+    zobj = zarr.open('data/example.zarr')
+    print(zobj.shape)
 
-viewer = napari.view_image(img2)
-napari.run()
+    #the next two commented examples should work if our zarrs had .zarray...
+
+    # tst = nio.read_zarr_dataset('https://s3-far.jax.org/zarrtest/test7.zarr')
+    # print(tst)
+
+    img = da.from_zarr(zobj, chunks=(100,100,100))
+    print(img)
+
+    time1 = timeit.timeit('gaussian(img, sigma=(1,1,1))', number=1, globals=globals())
+    print(time1)
+    time2= timeit.timeit('dask_image.ndfilters.gaussian_filter(img, sigma=[100, 100, 100])', number=1, globals=globals())
+    gauss2=dask_image.ndfilters.gaussian_filter(img, sigma=[10, 10, 10])
+    print(gauss2)
+    print(time2)
+    print(img[10:15,10:15,10:15].compute())
+    print(gauss2[10:15,10:15,10:15].compute())
+    # img2 = da.from_zarr('s3-server.org/bucket-name/zarr-name/1/1/10/0')
+
+    viewer = napari.view_image(img)
+    napari.run()
 
 
